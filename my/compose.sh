eca4170f5bae742eca9fae2d46b0588e0fe9cf39
Lyuboslav Petrov <petrov.lyuboslav@gmail.com>
2024-02-10 15:36:00 +0200

    Brought back to life

--- my/compose.sh
+++ my/compose.sh
@@ -5,5 +5,8 @@ docker compose \
 	-f docker-compose.gpu.yaml \
 	-f docker-compose.api.yaml \
 	-f docker-compose.data.yaml \
-        $@
+	-f docker-compose.litellm.proxy.yaml \
+    $@
 
+
+# docker compose -f docker-compose.local.yaml $@
